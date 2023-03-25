df36b289f256028aad867c4ef93d5328d9c947ad
[mayank pradhan] <mayankpradhan115@gmail.com>
2023-03-25 15:33:02 +0530

    commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Heu")
+const http = requires("http")
+const server = http.createServer(()=>{
+
+})
+
+server.listen(5000, (req, res)=>{
+    
+    if(req.url === "/"){
+        res.end(
+            <h1>Home</h1>
+        )
+    }
+    else if (url === "/about"){
+        res.end(
+            <h1>About</h1>
+        )
+    }
+    else{
+        <h1>Page not found</h1>
+    }
+
+})
